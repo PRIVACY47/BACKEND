e6d15349db590f7320c788c1bf54e5d2b98062cd
sivaramireddy <baramsivarami@gmail.com>
2023-01-29 23:03:15 +0530

    correcting notification error

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require('./db_connect.js')
 app.listen(3000,(req,res)=>{console.log("app startd on port 300")})
 
 
-app.use("/Notifications",notificationRouter)
+app.use("/notifications",notificationRouter)
 app.get('/' , (req,res) => {
    res.json({message : 'server is runnig'})
 })
