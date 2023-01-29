d61eda4ed6f03256e377f6bb6895e5b09cca8ad9
siva0 <71169645+baramsivaramireddy@users.noreply.github.com>
2023-01-29 23:26:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('./db_connect.js')
 app.listen(3000,(req,res)=>{console.log("app startd on port 300")})
 
 
-app.use("notifications",notificationRouter)
+app.use("/notifications",notificationRouter)
 app.get('/' , (req,res) => {
    res.json({message : 'server is runnig'})
 })
