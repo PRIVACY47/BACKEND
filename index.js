9717d4600f5453db3c0ae43098d29177b075a5f1
sivaramireddy <baramsivarami@gmail.com>
2023-01-29 23:13:58 +0530

    correcting notification error

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const notificationRouter = require('./Routes/NotificationRoute.js');
 const UserRouter = require('./Routes/UserRoute.js');
-
+const cors = require('cors')
 
 const app = express();
 app.use(express.json())
 app.use(morgan('tiny'))
-require('./db_connect.js')
+app.use(cors())
 
+require('./db_connect.js')
 
 app.listen(3000,(req,res)=>{console.log("app startd on port 300")})
 
